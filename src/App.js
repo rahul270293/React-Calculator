166ee7402c821e4e47160a7a91f7c3dce1bb9e50
rahul2702 <rahul270293@gmail.com>
2020-07-02 22:45:05 +0530

    first push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'
+import KeyPad from './Components/keyPad'
+import Output from './Components/Output'
 
-function App() {
+
+class App extends Component  {
+   state={
+     result: ''
+   }
+  buttonPressed=buttonName=>{
+    if(buttonName=== '='){
+      this.calculator()
+    } else if(buttonName==='C'){
+        this.reset()
+    }else if(buttonName==='CE'){
+      this.backspace()
+    }
+    else
+    this.setState({
+      result: this.state.result + buttonName
+    })
+  }
+ reset=()=>{
+   this.setState({
+     result:''
+   })
+ }
+
+ backspace=()=>{
+   this.setState({
+     result:this.state.result.slice(0,-1)
+   })
+ }
+
+  calculator=()=>{
+    try{
+      this.setState({
+        result:(eval(this.state.result) ||'') + " "
+      })
+    }catch(e){
+       this.setState({
+         result:"error"
+       })
+    }
+    
+  }
+  
+render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calc-body">
+        <div className="calcy"><b>CALCULATOR</b></div>
+      <Output result={this.state.result}/>
+      <KeyPad buttonPressed={this.buttonPressed}/>
+      </div>
+      
     </div>
   );
+}
+  
+  
 }
 
 export default App;
